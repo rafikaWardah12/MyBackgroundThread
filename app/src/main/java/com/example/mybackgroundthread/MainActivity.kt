f011970a172e3b1dabe5603f391b08ad3c1ce6f8
rafikawardah <rafika.wardah12@gmail.com>
2024-01-22 14:39:10 +0700

    Simulation the function of Ui thread with executor

--- app/src/main/java/com/example/mybackgroundthread/MainActivity.kt
+++ app/src/main/java/com/example/mybackgroundthread/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.mybackgroundthread
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
+import android.widget.TextView
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStart = findViewById<Button>(R.id.btn_start)
+        val tvStatus = findViewById<TextView>(R.id.tv_status)
+
+        val executor = Executors.newSingleThreadExecutor()
+        val handler = Handler(Looper.getMainLooper())
+
+        btnStart.setOnClickListener {
+            executor.execute {
+                try {
+                    //simulasi proces kompressing
+                    for (i in 0..10) {
+                        Thread.sleep(500)
+                        val percentage = 1 * 10
+
+                        handler.post {
+                            //Update Ui in Main thread
+                            if (percentage == 100) {
+                                tvStatus.setText(R.string.task_completed)
+                            } else {
+                                tvStatus.text =
+                                    String.format(getString(R.string.compressing), percentage)
+                            }
+                        }
+                    }
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+            }
+        }
     }
 }
